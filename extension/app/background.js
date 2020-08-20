1b4b3c37db7d591f04ee8f44e1621c8a38c40010
mjsamuel <matthew.samuel@outlook.com.au>
2020-08-20 18:51:23 +1000

    Updated project structure for webpack

--- extension/app/background.js
+++ extension/app/background.js
@@ -1,3 +1,6 @@
+const Web3 = require('web3');
+
+// Listens for whenever a tab changes
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   let url = changeInfo["url"]
   if(url != undefined) {
