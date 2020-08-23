fe978362f1d67ae74990621f1d9924e8782bea8d
mjsamuel <matthew.samuel@outlook.com.au>
2020-08-23 16:54:08 +1000

    Implemented link to Ganache

--- extension/app/background.js
+++ extension/app/background.js
@@ -1,9 +1,21 @@
 const Web3 = require('web3');
 
+// Creating connection to our Ganache personal blockchain
+var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
+
 // Listens for whenever a tab changes
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   let url = changeInfo["url"]
   if(url != undefined) {
-    console.log(url)
+    console.log(url);
+    listAccounts()
   }
 });
+
+// Prints the public address of all accounts in the blockchain
+function listAccounts() {
+  web3.eth.getAccounts()
+    .then(fetchedAccounts=> {
+      console.log(fetchedAccounts);
+    });
+}
