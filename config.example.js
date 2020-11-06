93b17cc8822dcdce6bd8f65a10918c74cab1679d
mjsamuel <matthew.samuel@outlook.com.au>
2020-11-06 15:29:01 +1100

    Removed previous Solidity code

--- config.example.js
+++ config.example.js
@@ -1,3 +1,2 @@
 export const IPNS_ADDRESS = '/ipns/QmWt5NiTyRwWWJy8EA12VuPvuvgGn8g3265FRahKTpwo6o';
 export const IPNS_KEY = 'QmWt5NiTyRwWWJy8EA12VuPvuvgGn8g3265FRahKTpwo6o';
-export const DEFAULT_ETH_ACCOUNT = '0x561D1D62083eBE58FFdcCBD283791f98d19a0AF0'
--- contract_backend/migrations/1_initial_migration.js
+++
@@ -1,7 +0,0 @@
-const Migrations = artifacts.require("Migrations");
-const Adblock = artifacts.require("Adblock");
-
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
-  deployer.deploy(Adblock);
-};
